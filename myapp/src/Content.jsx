7b0b69f2e7878bd8396708db0dbcfee380a2789b
arunprasadh7 <m.arunprasadh96@gmail.com>
2024-03-28 09:53:38 +0530

    4 React Click Events

--- myapp/src/Content.jsx
+++ myapp/src/Content.jsx
@@ -1,7 +1,19 @@
 const Content = () => {
+  const handleClick = () => console.log(`Subscribed`);
+
+  const clickedBy = (name) => console.log(`Clicked by ${name}`);
+
+  const clickEvent = (e) => console.log(e.target.innerText);
+
+  const doubleTap = () => console.log(`You have double clicked`);
+
   return (
     <main>
       <p>Content Space</p>
+      <button onClick={handleClick}>Subscribe</button>
+      <button onClick={() => clickedBy("Arun")}>Clicked By</button>
+      <button onClick={(e) => clickEvent(e)}>Click Event Text</button>
+      <button onDoubleClick={doubleTap}>Double Click</button>
     </main>
   );
 };
